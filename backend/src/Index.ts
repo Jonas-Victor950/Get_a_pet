5b9f03274bd1e189de6e34a7758551b7c42ac0dc
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2023-03-06 11:26:54 -0300

    Starting the project

---
+++ backend/src/Index.ts
@@ -0,0 +1,27 @@
+// ENV variable
+require("dotenv").config();
+import path from "path";
+
+import express from "express";
+import cors from "cors";
+
+async function main() {
+  const app = express();
+  const port = 3000;
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.static("public"))
+
+  app.listen(port, async () => {
+    Logger.info(`🚀 Projeto rodando no endereço: http://127.0.0.1:${port}`);
+
+    mongoDB.createConection();
+  });
+}
+
+main().catch((error) => {
+  Logger.error("🥵 Erro!");
+  Logger.error(error);
+});
