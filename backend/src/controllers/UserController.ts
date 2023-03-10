98e06b2d0c89e3b3524fb84a9268e0cd232afddd
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2023-03-10 11:13:37 -0300

    Validations user

---
+++ backend/src/controllers/UserController.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import { Request, Response } from "express";
+import Logger from "../database/logger";
+import { IUser, User } from "../models/User";
+
+const UserController = {
+  async createUser(req: Request, res: Response) {
+    const { name, email, phone, password, confirmpassword } = req.body;
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é obrigatório!" });
+      return;
+    }
+
+    if (!email) {
+      res.status(422).json({ message: "O e-mail é obrigatório!" });
+      return;
+    }
+
+    if (!phone) {
+      res.status(422).json({ message: "O telefone é obrigatório!" });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: "A senha é obrigatória!" });
+      return;
+    }
+
+    if (!confirmpassword) {
+      res
+        .status(422)
+        .json({ message: "A confirmação de senha é obrigatória!" });
+      return;
+    }
+
+    if (password != confirmpassword) {
+      res
+        .status(422)
+        .json({ message: "A senha e a confirmação precisam ser iguais!" });
+      return;
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+      res.status(422).json({ message: "Por favor, utilize outro e-mail!" });
+      return;
+    }
+  },
+};
