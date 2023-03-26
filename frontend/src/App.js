74e86308362e8a115532e7bcc996fe23a0a9277d
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2023-03-26 10:48:46 -0300

    Structuring Navbar and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 // Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// Components
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+
 // pages
 import Login from "./components/pages/Auth/Login";
 import Register from "./components/pages/Auth/Register";
@@ -10,11 +14,13 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
           <Route path="/" element={<Home />}></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
