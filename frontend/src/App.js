9e5be587fa9a20305f0f0a26e9dc10c2355e8310
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2023-03-26 10:27:43 -0300

    Inserting React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// pages
+import Login from "./components/pages/Auth/Login";
+import Register from "./components/pages/Auth/Register";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Get A Pet</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
