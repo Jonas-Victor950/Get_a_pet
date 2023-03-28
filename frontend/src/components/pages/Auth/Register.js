92c5766fef47ef39982f11ef0dd1943d7a2d8db2
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2023-03-28 19:14:24 -0300

    Creating user object

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "../../form/Form.module.css";
 import Input from "../../form/Input";
 
 const Register = () => {
-  function handleChange(e) {}
+  const [user, setUser] = useState({});
+
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
   return (
     <section className={styles.form_container}>
       <h1>Register</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Input
           text="Nome"
           type="text"
